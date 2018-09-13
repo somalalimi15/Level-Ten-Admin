9de829ec019221ca17f4d4c6655806590bf933fa
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-09-13 19:58:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,14 @@
 const Discord = require('discord.js');
-const client = new Discord.Client;
+const client = new Discord.Client();
 
-client.login(process.env.BOT_TOKEN)
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);  //لا تحط التوكن حقك هنا
